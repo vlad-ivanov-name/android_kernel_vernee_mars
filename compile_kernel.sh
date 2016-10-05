3ebaca164905106bbce9b4a2441d64b8052f655d
Alberto Guanti <albertoguanti@gmail.com>
2016-10-05 21:54:56 +0200

    compile_kernel.sh: Add a script to compile the kernel

---
+++ compile_kernel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ARCH=arm64
+export CCOMPILE=$CROSS_COMPILE
+export CROSS_COMPILE=aarch64-linux-android-
+export PATH=$PATH:../aarch64-linux-android-4.9/bin
+
+make -j8 clean
+make -j8 k11ta_a_defconfig
+make -j8
