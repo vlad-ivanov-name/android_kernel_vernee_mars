55a21f66034c50f10377df4dc548489d54978738
Alberto Guanti <albertoguanti@gmail.com>
2016-12-26 21:27:02 +0100

    arch: arm64: add custom defconfig for k11ta_a
    * in order to not mess with the original one

--- compile_kernel.sh
+++ compile_kernel.sh
@@ -6,5 +6,5 @@ export CROSS_COMPILE=aarch64-linux-android-
 export PATH=$PATH:../aarch64-linux-android-4.9/bin
 
 make -j8 clean
-make -j8 k11ta_a_defconfig
+make -j8 custom_k11ta_a_defconfig
 make -j8
